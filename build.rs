804fc046f9e9efea6040afac0b2c8b0df69df045
Reilly Wood <reilly.wood@icloud.com>
2024-04-03 18:14:03 -0700

    Iconify v0.3.0 with cross-rs fix

--- build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    // https://github.com/wrapperup/iconify-rs/issues/38
-    if is_docker::is_docker() {
-        println!("cargo:rustc-env=ICONIFY_CACHE_DIR=/tmp/iconify/");
-    }
-}
